72ba8d86cd3cf932072d409c39367059f5182740
oivindr <oivind.rui@gmail.com>
2015-02-21 16:57:06 +0100

    - First version of assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 - cached computation and use of lexical scoping
+## Two functions constitute the functionality
+## 1 - makeCacheMatrix - construct the matrix from and ordinary R matrix
+## 2 - cacheSOlve - returns a cached inverse matrix as long as original 
+##     matrix has not changed since last comptation of inverse
 
-## Write a short comment describing this function
+## The function, `makeCahceMatrix` creates a special "matrix", which is
+## really a list containing a function to
+##
+## 1.  set the value of the matrix
+## 2.  get the value of the matrix
+## 3.  set the value of the inverse matrix
+## 4.  get the value of the inverse matrix
+##
+## The matrix is assumed to be quadratic & invertible
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrixr"
+## created with the above function. However, it first checks to see if the
+## inverse has already been calculated. If so, it `get`s the inverse from the
+## cache and skips the computation. Otherwise, it calculates the inverse of
+## the matrix and sets the value of the inverse in the cache via the `setInverse`
+## function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if(!is.null(i)) {
+                message("getting cached inverse")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data)
+        x$setinverse(i)
+        i
 }
